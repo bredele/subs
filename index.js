1b8af375acb87f9eb9b68ba69061747a49302d57
bredele <olivier.wietrich@gmail.com>
2013-11-26 08:34:16 -0700

    expose subs module

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var supplant = require('supplant');
 
 
 /**
+ * Expose 'Subs'
+ * @type {[type]}
+ */
+
+module.exports = Substitution;
+
+
+/**
  * Node text substitution constructor.
  * @param {HTMLElement} node  type 3
  * @param {Store} store 
  */
 
-module.exports = function Substitution(node, store) { //may be use an adapter
+function Substitution(node, store) { //may be use an adapter
   this.node = node;
   this.store = store;
   //cache text template
   this.text = node.nodeValue;
 
   this.exprs = supplant.attrs(this.text);
-  for(var l = this.exprs.length; l--;){ //TODO: do own each package with a fast loop
+  for(var l = this.exprs.length; l--;) {
     var expr = this.exprs[l];
     var _this = this;
     store.on('change ' + expr, function(){ //TODO: have emitter with scope
@@ -22,7 +30,7 @@ module.exports = function Substitution(node, store) { //may be use an adapter
     });
   }
   this.apply();
-};
+}
 
 
 /**
