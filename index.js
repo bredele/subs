87c30298ddd236e73c4a13cf7acb7f365a5f8630
bredele <olivier.wietrich@gmail.com>
2013-12-08 19:13:58 -0700

    tests with new supplant

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ var supplant = require('supplant');
  * @param {Store} store 
  */
 
-module.exports = function(node, store) { //may be use an adapter
+module.exports = function(node, store) {
   var text = node.nodeValue,
       exprs = supplant.attrs(text);
   for(var l = exprs.length; l--;) {
-  	// var expr = exprs[l];
-  	// if(expr[0] === '{') {		
-
+    //when destroy binding, we should do off store
     store.on('change ' + exprs[l], function() {
       replace(node, text, store);
     });
